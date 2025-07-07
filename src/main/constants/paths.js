7002527068be5ab7596f6fd6af3ceae09f800108
Anas Hisham <anshisham123@gmail.com>
2025-07-07 15:34:53 +0300

    chore: fix installer setup by updating icon and output name

--- src/main/constants/paths.js
+++ src/main/constants/paths.js
@@ -4,9 +4,9 @@ import { app } from 'electron'
 const DEFAULT_SAVE_PATH = app.getPath('userData')
 
 export const PATHS = {
-    ICON: path.join('public/icons/icon.ico'),
-    PRELOAD: path.join('../preload/index.js'),
-    DIST_HTML: path.join('../renderer/index.html'),
-    LOG_FILE_NAME: 'errors.log',
-    DEFAULT_SAVE_PATH
+  ICON: path.join('public/icons/icon.ico'),
+  PRELOAD: path.join('../preload/index.js'),
+  DIST_HTML: path.join('../renderer/index.html'),
+  LOG_FILE_NAME: 'errors.log',
+  DEFAULT_SAVE_PATH
 }
